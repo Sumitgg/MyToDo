e31bdfe50181224f4d7877d5e73b0a1555970b06
Sumit Ganju <sganju@sumits-macbook-pro.local>
2018-01-10 16:58:14 -0500

    Added functionality to add and remove tasks, mark the to-do tasks as done when tapped on.

--- MyToDo/ViewController.swift
+++ MyToDo/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "To-Do"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.didTapAddItemButton(_:)))
     }
     
     private var todoItems = ToDoItem.getMockData()
@@ -53,6 +56,52 @@ class ViewController: UITableViewController {
             item.done = !item.done
             
             tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    @objc func didTapAddItemButton(_ sender: UIBarButtonItem)
+    {
+        // Create an alert
+        let alert = UIAlertController(
+            title: "New to-do item",
+            message: "Insert the title of the new to-do item:",
+            preferredStyle: .alert)
+        
+        // Add a text field to the alert for the new item's title
+        alert.addTextField(configurationHandler: nil)
+        
+        // Add a "cancel" button to the alert. This one doesn't need a handler
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        // Add a "OK" button to the alert. The handler calls addNewToDoItem()
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+            if let title = alert.textFields?[0].text
+            {
+                self.addNewToDoItem(title: title)
+            }
+        }))
+        
+        // Present the alert to the user
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func addNewToDoItem(title: String)
+    {
+        // The index of the new item will be the current item count
+        let newIndex = todoItems.count
+        
+        // Create new item and add it to the todo items list
+        todoItems.append(ToDoItem(title: title))
+        
+        // Tell the table view a new row has been created
+        tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+    }
+    // Fumction that allows the user to remove a todo task from the list on swipping from the left
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if indexPath.row < todoItems.count
+        {
+            todoItems.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .top)
         }
     }
 }
